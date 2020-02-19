fe031c90791e08d041fab2f4b127ccb864485ce3
Frank X <frankyaoxiao@gmail.com>
2020-02-19 18:30:30 +0000

    update our strategy

--- team5.py
+++ team5.py
@@ -10,6 +10,7 @@ team_name = 'Collusion' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
+
 import random
 
     
